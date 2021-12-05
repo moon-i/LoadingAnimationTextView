d8b1f2a4fd6e5bac6e1ee7bc0521807b5be800b3
SEOMOON CHOI <seomoonchoi@SEOMOONui-MacBookAir.local>
2021-12-05 22:51:30 +0900

    [LATV-1] LoadingTextView 생성 및 사용 예시 연결
    - startFirst : 위에서 랜딩해서 내려오는 애니메이션
    - setText : 애니메이션 없이 텍스트 설정
    - stopFadeInLoading : text 위에서 랜딩 후 멈추는 함수(호출시 람다 작성시 멈춘후 동작정의 가능)
    - stopFadeOutLoading : text 밑으로 사라진 후 멈추는 함수(호출시 람다 작성시 멈춘후 동작 정의 가능)
    - startLoadint : 로딩 시작
    - startOneCycleAnimation : text밑으로 사라지고 위에서 내려오는 한 사이클 실행 함수

--- app/src/main/java/com/moon/loadinganimationtextview/MainActivity.kt
+++ app/src/main/java/com/moon/loadinganimationtextview/MainActivity.kt
@@ -2,10 +2,42 @@ package com.moon.loadinganimationtextview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.moon.loadinganimationtextview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.setText(
+            LoadingTextView.Companion.TextAttribute(
+                "HelloWorld!",
+                28f,
+                "#000000"
+            )
+        )
+
+        binding.btnStart.setOnClickListener{
+            binding.textView.startLoading()
+        }
+
+        binding.btnNewValue.setOnClickListener {
+            binding.textView.stopFadeOutLoading {
+                binding.textView.startFirst(
+                    LoadingTextView.Companion.TextAttribute(
+                        "NewText",
+                        30f,
+                        "#123456"
+                    )
+                )
+            }
+        }
+
+        binding.btnStop.setOnClickListener {
+            binding.textView.stopFadeInLoading()
+        }
     }
 }
